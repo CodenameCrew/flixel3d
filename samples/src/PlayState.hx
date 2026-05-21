2e3f50135c76d05ca17e990f6834fe502854ab41
kitzsh <110337474+kitzsh@users.noreply.github.com>
2026-05-21 19:03:05 +1000

    Add sample project

--- samples/src/PlayState.hx
+++ samples/src/PlayState.hx
@@ -1,21 +1,35 @@
 package;
 
-import flixel3d.Flx3DScene;
+import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel3d.Flx3DModel;
+import flixel3d.Flx3DScene;
 
-class PlayState extends flixel.FlxState {
-	override public function create():Void {
+class PlayState extends FlxState {
+	override public function create() {
 		super.create();
 
-		var view = new Flx3DScene(0, 0, FlxG.width, FlxG.height);
-		add(view);
+		var txtBehind = new FlxText(0, 0, 0, "This text is behind the Flx3DScene", 20);
+		txtBehind.color = 0xFFAAFFFF;
+		txtBehind.screenCenter();
+		txtBehind.y -= 40;
+		add(txtBehind);
 
-		var model = new Flx3DModel();
-		model.loadMeshes("assets/models/testroom.obj");
-		view.add(model);
+		var scene = new Flx3DScene(0, 0);
+		add(scene);
+
+		var txtFront = new FlxText(0, 0, 0, "This text is in front of the Flx3DScene", 20);
+		txtFront.color = 0xFFFFAAFF;
+		txtFront.screenCenter();
+		txtFront.y += 40;
+		add(txtFront);
+
+		var suzanne = new Flx3DModel(0, 0, -10).loadMeshes("assets/SuzanneMonkey.obj");
+		scene.objects.add(suzanne);
+		suzanne.angularVelocity3D.y = 30;
 	}
 
-	override public function update(elapsed:Float):Void {
+	override public function update(elapsed:Float) {
 		super.update(elapsed);
 	}
 }
